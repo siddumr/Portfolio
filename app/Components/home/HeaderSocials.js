4c8a06ae3e615b4eef5349615e999095f23bf24e
siddumr <mrsiddu2112@gmail.com>
2025-05-21 18:42:34 +0530

    update

--- app/Components/home/HeaderSocials.js
+++ app/Components/home/HeaderSocials.js
@@ -5,19 +5,21 @@ const HeaderSocials = () => {
   return (
   <div className='home__socials'>
         <a href='https://www.linkedin.com/in/siddarthan-m-r-423546258' className='home__social-link' target='_blank'>
-        <i class="fa-brands fa-linkedin"></i>
-        </a>
-     <a href="https://leetcode.com/your-username" className="home__social-link" target="_blank" rel="noopener noreferrer">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 1024 1024"
-      fill="currentColor"
-    >
-      <path d="M713.6 842.7L529 1024 193.3 691.3l117.4-118.2 123.2 124.3 191.1-193.4L430.3 309.5l118.6-120 305.2 305.9c41.2 41.3 66.6 96.3 66.6 158.3s-25.4 117-66.6 158.3L713.6 842.7zM642.6 773.4l128.7-129.3c17.6-17.7 27.4-41.2 27.4-66.4s-9.8-48.7-27.4-66.4L596.6 390.2 540.9 446l106.7 107.9-306.1 310-58.3-58.8L317 745.4l157.6 158.9 167.9-171zM387.2 508.3L217.7 336.7l230-231.9 174.5 177.2-61 61.8-113.3-115.1-114.2 115.1 167.2 169.5-114.4 115z"/>
-    </svg>
+        <i class="fa-brands fa-linkedin"></i></a>
+     <a href="https://leetcode.com/your-username"
+  className="home__social-link"
+  target="_blank"
+  rel="noopener noreferrer">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="currentColor">
+    <path d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.266 5.266 0 0 0-1.209 2.104 5.35 5.35 0 0 0-.125.513 5.527 5.527 0 0 0 .062 2.362 5.83 5.83 0 0 0 .349 1.017 5.938 5.938 0 0 0 1.271 1.818l4.277 4.193.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.378 1.378 0 0 0-1.951-.003l-2.396 2.392a3.021 3.021 0 0 1-4.205.038l-.02-.019-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.68 2.68 0 0 1 .066-.523 2.545 2.545 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0zm-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382 1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382 1.38 1.38 0 0 0-1.38-1.382z"/>
+  </svg>
 </a>
+
 
         <a href='https://github.com/siddumr' className='home__social-link' target='_blank'>
         <i class="fa-brands fa-github"></i>
